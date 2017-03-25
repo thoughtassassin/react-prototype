f971d9dc45bca997feb746e238f435b72468a763
David Krause <david.krause@egov.com>
2017-03-25 17:54:13 -0500

    Add validate to Input class

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 export class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.valid = false;
+
+        this.handleBlur = this.handleBlur.bind(this);
+    }
+    
+    validate(value) {
+        if (value === null || value === '') {
+            return false;
+        }
+        return true;
+    }
+    
+    handleBlur(event) {
+        this.valid = this.validate(event.target.value);
+        console.log("input valid is: ", this.valid);
+    }
+
     render() {
         return (
             <div className="form-group">
@@ -15,6 +34,7 @@ export class Input extends React.Component {
                         className="form-control" 
                         name={this.props.name}
                         placeholder={this.props.placeholder}
+                        onBlur={this.handleBlur}
                         onChange={this.props.handleChange} />
             </div>
         );
